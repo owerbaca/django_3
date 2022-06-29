60e5ecc106b5d2b610c7d28f9b1f0e3fc855b74e
hunterdev732 <hunterdev732@gmail.com>
2022-06-29 20:50:34 -0300

    proyecto_3

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\owerb\documents\proyectos_2022\projecto_django3\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
